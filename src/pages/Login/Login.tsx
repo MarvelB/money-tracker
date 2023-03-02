ff9e5c24175a4c5767c89e2f4f25cbd72f7d275d
Bryan <bryan@devlift.io>
2023-03-01 22:19:20 -0500

    added initial code for login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,46 @@
+import { useState } from 'react';
+
 // @ts-ignore: Cannot find css module
 import styles from './Login.module.css';
 
 interface LoginProps {}
 
 const Login = ({ }: LoginProps) => {
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(email, password);
+  }
+
   return (
-    <div>
+    <form className={styles["login-form"]} onSubmit={handleSubmit}>
       <h2>Login</h2>
-    </div>
+
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <button className="btn">Login</button>
+
+    </form>
   );
 }
 
