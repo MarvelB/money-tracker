e41a05a8748687252393c875afb5402689e81671
Bryan <bryan@devlift.io>
2023-03-03 07:08:47 -0500

    added code to wait for isAuthReady

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import NavBar from 'components/NavBar/NavBar';
+import { useAuthContext } from 'hooks/useAuthContext';
 import Home from 'pages/Home/Home';
 import Login from 'pages/Login/Login';
 import Signup from 'pages/Signup/Signup';
@@ -6,28 +7,33 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
+
+  const { isAuthReady } = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
+      {isAuthReady && (
+        <BrowserRouter>
 
-        <NavBar />
+          <NavBar />
 
-        <Switch>
+          <Switch>
 
-          <Route path="/login">
-            <Login />
-          </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
 
-          <Route path="/signup">
-            <Signup />
-          </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
 
-          <Route exact path="/">
-            <Home />
-          </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
 
-        </Switch>
-      </BrowserRouter>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
